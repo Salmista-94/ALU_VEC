b17897a61455340d642cb4a9d199b3339a3d99a3
alejandrogroso@hotmail.com <Salmista-94>
2016-06-11 01:51:21 -0300

    operadores alternativos

---
+++ suma_resta_alternativo.vhd
@@ -0,0 +1,68 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:33:38 05/28/2016 
+-- Design Name: 
+-- Module Name:    dec_hex_7_SEG - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+entity sum_res2 is
+    generic (SIZE: integer range 1 to 32:= 16);
+    Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);-- declararlo como array (x[n])
+          over : out  STD_LOGIC;  
+          sal : out  STD_LOGIC_VECTOR (SIZE-1 downto 0));  
+end sum_res2;
+
+
+
+architecture RESTAR of sum_res2 is
+begin       
+        
+sal <= op1 - op2;
+IF (sal<0 AND op1>0 AND op2<0) OR (sal>0 AND op1<0 AND op2>0) OR (sal=(other=>"0") AND op1=sal="1"(other=>"0")) THEN
+    over := '1';
+ELSE
+    over := '0';
+END IF;
+    
+end RESTAR;
+
+
+
+
+
+
+
+architecture SUMAR of sum_res2 is
+begin       
+        
+sal <= op1 + op2;
+IF (sal<0 AND op1>0 AND op2>0) OR (sal>0 AND op1<0 AND op2<0) OR (sal=(other=>"0") AND op1=sal="1"(other=>"0")) THEN
+    over := '1';
+ELSE
+    over := '0';
+END IF;
+    
+end SUMAR;
