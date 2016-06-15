45074c45e10c0dcc66d1be9745db0c733513601a
alejandrogroso@hotmail.com <Salmista-94>
2016-06-15 12:01:10 -0300

    ahora usando señal auxiliar y 'process()'

--- prod_div.vhd
+++ prod_div.vhd
@@ -31,8 +31,9 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity prod_div1 is
     generic (SIZE: integer range 1 to 32:= 16);
     Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);-- declararlo como array (x[n])
-	        sal : out  STD_LOGIC_VECTOR (SIZE downto 0)
-            c,z : out  STD_LOGIC);   	 
+	        sal : out  STD_LOGIC_VECTOR (SIZE-1 downto 0)
+            c,z : out  STD_LOGIC
+            zeroDivision : out  STD_LOGIC);   	 
 end prod_div1;
 
 
@@ -40,11 +41,16 @@ end prod_div1;
 
 
 architecture MULTIPLICAR of prod_div1 is
+tmp : out  STD_LOGIC_VECTOR (SIZE downto 0);
 begin       
 
-sal <= op1 * op2;
-c := '1' when sal(SIZE) /= op1(SIZE-1) and op1(SIZE-1) = op2(SIZE-1) else '0';
-z := '1' when sal = (others => '0') else '0';
+process(op1, op2)
+begin
+    tmp <= (op1(SIZE-1)& op1) * op2;
+    sal <= tmp(SIZE-1 to 0);
+    c := '1' when tmp(SIZE) /= op1(SIZE-1) and op1(SIZE-1) = op2(SIZE-1) else '0';
+    z := '1' when sal = (others => '0') else '0';
+end process;
         
 end MULTIPLICAR;
 
@@ -56,9 +62,16 @@ end MULTIPLICAR;
 architecture DIVIDIR of prod_div1 is
 begin       
 
-sal <= op1 / op2;
-c := '1' when sal(SIZE) /= op1(SIZE-1) and op1(SIZE-1) = op2(SIZE-1) else '0';
-z := '1' when sal = (others => '0') else '0';
+process(op1, op2)
+begin
+    if op2 = (others => '0') then 
+        zeroDivision <= '1';
+    else
+        zeroDivision <= '0';
+        sal <= op1 / op2;
+        z := '1' when sal = (others => '0') else '0';
+    end if ;
+end process;
         
 end DIVIDIR;
 
