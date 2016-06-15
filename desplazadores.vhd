29b7437860189e216bf4719178f4bb395f404b05
alejandrogroso@hotmail.com <Salmista-94>
2016-06-15 13:49:26 -0300

    ahora funcionan los desplazadores!

--- desplazadores.vhd
+++ desplazadores.vhd
@@ -48,30 +48,6 @@ end DESPLAZAR_D;
 
 
 
-entity desplazador2 is
-    generic (SIZE: integer range 1 to 32:= 16);
-    Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);-- declararlo como array (x[n])
-          over : out  STD_LOGIC;  
-          sal : out  STD_LOGIC_VECTOR (SIZE-1 downto 0));  
-end desplazador2;
-
-
-
-
-architecture DESPLAZAR_D of prod_div2 is
-begin       
-        
-sal <= op1 + op2;
-IF (sal<0 AND op1>0 AND op2>0) OR (sal>0 AND op1<0 AND op2<0) OR (sal=(other=>"0") AND op1=sal="1"(other=>"0")) THEN
-    over := '1';
-ELSE
-    over := '0';
-END IF;
-    
-end DESPLAZAR_D;
-
-
-
 
 
 architecture DESPLAZAR_I of desplazador1 is
@@ -84,17 +60,84 @@ end DESPLAZAR_I;
 
 
 
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:33:38 05/28/2016 
+-- Design Name: 
+-- Module Name:    dec_hex_7_SEG - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity desplazador1 is
+    generic (SIZE: integer range 1 to 32:= 16);
+    Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);   -- declararlo como array (x[n])
+            c : out  STD_LOGIC;                                 -- podría implementarlo sin 'CARRY'
+            sal : out  STD_LOGIC_VECTOR (SIZE downto 0));    
+end desplazador1;
 
 
-architecture DESPLAZAR_I of desplazador2 is
+
+
+
+architecture DESPLAZAR_D of desplazador1 is
+tmp1 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+tmp2 : out  STD_LOGIC_VECTOR (SIZE downto 0);
 begin       
+
+process(op1, op2)
+    begin       
+    tmp1 <= (0&op1)/2;
+    tmp2 <= (0&op2)/2;
+    sal1 <= tmp1(SIZE-1 to 0);
+    sal2 <= tmp2(SIZE-1 to 0);
+
+    --podría tambien hacer 'carries' y 'zeros' para cada elemento del bus...
+    c := '1' when tmp1(SIZE) = '1' or tmp2(SIZE) = '1' else '0';
+end process;
         
-sal <= op1 - op2;
-IF (sal<0 AND op1>0 AND op2<0) OR (sal>0 AND op1<0 AND op2>0) OR (sal=(other=>"0") AND op1=sal="1"(other=>"0")) THEN
-    over := '1';
-ELSE
-    over := '0';
-END IF;
-    
+end DESPLAZAR_D;
+
+
+
+
+
+architecture DESPLAZAR_I of rotador1 is
+tmp1 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+tmp2 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+begin       
+
+process(op1, op2)
+    begin       
+    tmp1 <= (0&op1)*2;
+    tmp2 <= (0&op2)*2;
+    sal1 <= tmp1(SIZE-1 to 0);
+    sal2 <= tmp2(SIZE-1 to 0);
+
+    --podría tambien hacer 'carries' y 'zeros' para cada elemento del bus...
+    c := '1' when tmp1(SIZE) = '1' or tmp2(SIZE) = '1' else '0';
+end process;     
+
+        
 end DESPLAZAR_I;
 
---
+++ desplazadores_alternativo.vhd
@@ -0,0 +1,56 @@
+
+entity desplazador2 is
+    generic (SIZE: integer range 1 to 32:= 16);
+    Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);-- declararlo como array (x[n])
+          c, z : out  STD_LOGIC;  
+          sal1, sal2 : out  STD_LOGIC_VECTOR (SIZE-1 downto 0));  
+end desplazador2;
+
+
+
+
+
+
+architecture DESPLAZAR_D of desplazador2 is
+tmp1 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+tmp2 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+begin       
+
+process(op1, op2)
+    begin
+    tmp1 <= (0&op1)/2;
+    tmp2 <= (0&op2)/2;
+    sal1 <= tmp1(SIZE-1 to 0);
+    sal2 <= tmp2(SIZE-1 to 0);
+
+    --podría tambien hacer 'carries' y 'zeros' para cada elemento del bus...
+    c := '1' when tmp1(SIZE) = '1' or tmp2(SIZE) = '1' else '0';
+    z := '1' when sal1 = (others => '0') or sal2 = (others => '0') else '0';
+end process;
+        
+end DESPLAZAR_D;
+
+
+
+
+
+architecture DESPLAZAR_I of desplazador2 is
+tmp1 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+tmp2 : out  STD_LOGIC_VECTOR (SIZE downto 0);
+begin     
+
+process(op1, op2)
+    begin
+    tmp1 <= (0&op1)*2;
+    tmp2 <= (0&op2)*2;
+    sal1 <= tmp1(SIZE-1 to 0);
+    sal2 <= tmp2(SIZE-1 to 0);
+
+    --podría tambien hacer 'carries' y 'zeros' para cada elemento del bus...
+    c := '1' when tmp1(SIZE) = '1' or tmp2(SIZE) = '1' else '0';
+    z := '1' when sal = (others => '0') else '0';
+end process;  
+
+        
+end DESPLAZAR_I;
+
