d1e16bde3f74888b463cb923efcc7d9d248af8b6
alejandrogroso@hotmail.com <Salmista-94>
2016-06-15 11:25:53 -0300

    ahora usando señal auxiliar y 'process()'

--- suma_resta.vhd
+++ suma_resta.vhd
@@ -31,18 +31,24 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity sum_res1 is
     generic (SIZE: integer range 1 to 32:= 16);
     Port ( op1, op2 : in  STD_LOGIC_VECTOR (SIZE-1 downto 0);-- declararlo como array (x[n])
-            sal : out  STD_LOGIC_VECTOR (SIZE downto 0);
+            sal : out  STD_LOGIC_VECTOR (SIZE-1 downto 0);
             c,z : out  STD_LOGIC);   
 end sum_res1;
 
 
 
 architecture SUMAR of sum_res1 is
+tmp : out  STD_LOGIC_VECTOR (SIZE downto 0);
 begin       
 
-sal := (op1(SIZE-1)& op1) + op2;
-c := '1' when sal(SIZE) /= op1(SIZE-1) and op1(SIZE-1) = op2(SIZE-1) else '0';
-z := '1' when sal = (others => '0') else '0';
+process(op1, op2)
+begin
+    tmp <= (op1(SIZE-1)& op1) + op2;
+    sal <= tmp(SIZE-1 to 0);
+    c := '1' when tmp(SIZE) /= op1(SIZE-1) and op1(SIZE-1) = op2(SIZE-1) else '0';
+    z := '1' when sal = (others => '0') else '0';
+end process;
+
         
 end SUMAR;
 
@@ -53,10 +59,15 @@ end SUMAR;
 
 
 architecture RESTAR of sum_res1 is
+tmp : out  STD_LOGIC_VECTOR (SIZE downto 0);
 begin       
 
-sal <= (op1(SIZE-1)& op1) - op2;
-c := '1' when sal(SIZE) = op2(SIZE-1) and op1(SIZE-1) /= op2(SIZE-1) else '0';
-z := '1' when sal = (others => '0') else '0';
+process(op1, op2)
+begin
+    tmp <= (op1(SIZE-1)& op1) - op2;
+    sal <= tmp(SIZE-1 to 0);
+    c := '1' when tmp(SIZE) = op2(SIZE-1) and op1(SIZE-1) /= op2(SIZE-1) else '0';
+    z := '1' when sal = (others => '0') else '0';
+end process;
         
 end RESTAR;
